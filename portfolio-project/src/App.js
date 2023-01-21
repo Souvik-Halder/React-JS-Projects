8cdfe69bed045668180bd5edcdf71d2c9faeae4e
Souvik Halder <souvik.halder2020@gift.edu.in>
2023-01-21 21:08:14 +0530

    upto aboutus section

--- portfolio-project/src/App.js
+++ portfolio-project/src/App.js
@@ -1,12 +1,17 @@
+import About from './components/About';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import SocialLinks from './components/SocialLinks';
 import logo from './logo.svg';
 
 function App() {
   return (
     <>
+    <div className=''></div>
   <Navbar/>
   <Home/>
+  <About/>
+  <SocialLinks/>
   </>
   );
 }
--- portfolio-project/src/components/Home.jsx
+++ portfolio-project/src/components/Home.jsx
@@ -3,16 +3,20 @@ import HeroImage from '../assets/heroImage.png'
 import {AiOutlineArrowRight} from 'react-icons/ai'
 function Home() {
   return (
-    <div>
-        <div>
-            <div>
-                <h2>I am a Full Stack Developer</h2>
-                <p> I have 3 years of experience building and design of software.
-                    Currently , I love to work on web application using technologies like
+    <div name="home" className='h-screen w-full bg-gradient-to-b from-black to-gray-800 via-black'>
+        <div className='max-w-screen-lg mx-auto flex flex-col items items-center justify-center h-full px-4 md:flex-row'>
+            <div className='flex flex-col justify-center h-full'>
+                <h2 className='text-4xl sm:text-7xl font-bold text-white my-2'>I am a Full Stack Developer</h2>
+                <p className='my-2 mx-3 text-gray-500 py-4 max-w-md'> I have 3 years of experience building and design of software.
+                    Currently ,tai I love to work on web application using technologies like
                     React, Tailwind , MongoDB , Node Js 
                 </p>
-                <div><button>
-                    Portfolio <span><AiOutlineArrowRight/></span> </button></div>
+                <div className=''><button className='bg-gradient-to-r from-cyan-500 to-blue-500 text-white w-fit  px-6 py-4 mt-4 flex items-center rounded-md cursor-pointer my-3'>
+                    Portfolio <span className='px-3 group-hover:rotate-90 duration-300'><AiOutlineArrowRight className='ml-1' /></span> </button>
+                    </div>
+            </div>
+            <div>
+              <img src={HeroImage} alt="my Profile" className='rounded-2xl mx-auto w-2/3 md:w-full ' />
             </div>
         </div>
     </div>
