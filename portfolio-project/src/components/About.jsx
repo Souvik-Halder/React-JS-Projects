44d405d2b69eedce0b9527f11a3e43d80a5fccc0
Souvik Halder <souvik.halder2020@gift.edu.in>
2023-01-22 07:25:53 +0530

    about section done

--- portfolio-project/src/components/About.jsx
+++ portfolio-project/src/components/About.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <div name="about" className='className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+        <div className=' max-w-screen-lg flex flex-col justify-center  p-4 w-full h-full mx-auto'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+            </div>
+            <div></div>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat dolorem mollitia voluptatibus tempora. Expedita, vitae quod! Corporis quis atque illo expedita repudiandae consequuntur ratione labore sed odio? Recusandae magnam necessitatibus voluptatum, ipsam quibusdam similique, sunt ad incidunt maiores natus iste illum assumenda quaerat minus corrupti fuga placeat numquam! Repudiandae, numquam.
+            </p>
+            <br />
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit autem velit nostrum ipsam ex aliquam, harum dicta maiores culpa iusto dolores quam recusandae officiis nisi, facere labore voluptate! Repellendus fugit distinctio nam nostrum ipsam neque, aliquam at pariatur est provident soluta? Explicabo, sit minima excepturi non quae impedit nulla obcaecati.
+            </p>
+        </div>
+    </div>
   )
 }
 
